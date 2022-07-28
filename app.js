16a65564e2149f707020730ebf51a0d25b0147f3
Ayange Felix <ayangefelix8@gmail.com>
2022-07-28 10:18:28 +0100

    add controllers, models, routes

--- app.js
+++ app.js
@@ -6,22 +6,29 @@ const bodyParser = require('body-parser');
 
 //db connection
 const dbConnect = require('./db/dbConnect');
+
+//routers
+const authRoutes = require('./routes/authRoute');
+
 //error handlers
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-
 app.use(bodyParser.json());
+app.get("/hello", (req, res) => {
+    res.status(200).send("<p>Hello you hit me</p>")
+});
+app.use('/api/v1/auth', authRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const start = async(req, res) => {
     try {
         await dbConnect(process.env.MONGO_URL)
-        app.listen(() => {
+        app.listen(port, () => {
             console.log(`App is starting on port ${port}...`);
         });
     } catch (error) {
@@ -30,3 +37,5 @@ const start = async(req, res) => {
 }
 
 start();
+
+module.exports = app;
---
+++ controllers/authController.js
@@ -0,0 +1,6 @@
+const User = require('../models/userModel');
+
+exports.register = async(req, res) => {
+    console.log("hello world");
+    res.status(200).send();
+};
