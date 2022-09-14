1e396365eb865d230e1449175480368860ae4dc7
Ayange Felix <ayangefelix8@gmail.com>
2022-09-14 09:34:40 +0100

    refactor mail sending

--- utils/sendResetPasswordEmail.js
+++ utils/sendResetPasswordEmail.js
@@ -1,15 +1,9 @@
 const nodemailer = require('nodemailer');
+const nodemailerConfig = require('../utils/nodemailerConfig');
 
 exports.sendResetPasswordEmail = async (email, name, passwordToken, callbackUrl) => {
 
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  const transporter = nodemailer.createTransport(nodemailerConfig);
 
   const resetURL =  `${callbackUrl}/reset-password?passwordToken=${passwordToken}&email=${email}`;
 
--- utils/sendVerificationEmail.js
+++ utils/sendVerificationEmail.js
@@ -1,15 +1,9 @@
 const nodemailer = require('nodemailer');
+const nodemailerConfig = require('../utils/nodemailerConfig');
 
 exports.sendVerificationEmail = async (email, name, verificationToken) => {
 
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  const transporter = nodemailer.createTransport(nodemailerConfig);
 
   const verifyEmail = `${process.env.USER_VERIFY_EMAIL}/verify-email?verificationToken=${verificationToken}&email=${email}`;
 
